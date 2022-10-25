71aa920f32ebc9c619e03b3a89c8a8d65baa9d60
Raquel Braga <87723504+Raquelsantos242@users.noreply.github.com>
2022-10-25 16:31:55 -0300

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,7 @@
+import math
+
+x = float(input('digite x: '))
+y = math.sqrt (x)
+
+print('a raiz quadrada de', x, 'é:',y)
+
---
+++ temp.py
@@ -0,0 +1,10 @@
+try:
+    x = float(input('digite um número > 0: '))
+    y = 1 / x
+    print('1/', x, '=',y)
+    
+except:
+    print('alguma entrada foi errada')
+    print('infelizmente, não pude fazer o cálculo')
+    
+print('FIM')
